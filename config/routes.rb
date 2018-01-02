2c517bba809dcdbe3b089012ed6931b3822dd3cd
BCrawfordScott <BrianCrawfordScott@gmail.com>
2018-01-02 12:40:46 -0500

    Adds jquery and js skeleton plus webpack config

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
-    resources :user, only: [ :create ]
+    resources :users, only: [ :create ]
     resources :session, only: [ :create, :destroy ]
   end
 end
