73774f6a914341dfa8a971a183e02d724d021bab
BCrawfordScott <BrianCrawfordScott@gmail.com>
2018-01-02 14:13:07 -0500

    Implements root component

--- frontend/aeternote.jsx
+++ frontend/aeternote.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store.js';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
+
   window.getState = store.getState; // testing
   window.dispatch = store.dispatch; // testing
-  ReactDOM.render(<h1>Welcome to æterNote</h1>, root);
+
+  ReactDOM.render(<Root store={ store } />, root);
 });
---
+++ frontend/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+      <h1>æterNote</h1>
+    </div>
+  );
+
+
+export default App;
