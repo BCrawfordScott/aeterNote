318673e20fa6ac40f1bd36f469a7c626f3d4d1bf
BCrawfordScott <BrianCrawfordScott@gmail.com>
2018-01-02 13:57:22 -0500

    Adds thunk

--- frontend/aeternote.jsx
+++ frontend/aeternote.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const root = document.getElementById('root');
+  window.getState = store.getState; // testing
+  window.dispatch = store.dispatch; // testing
   ReactDOM.render(<h1>Welcome to æterNote</h1>, root);
 });
