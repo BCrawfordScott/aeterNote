641db83aa802165a5e35cdbb3f3aec2d5750460d
BCrawfordScott <BrianCrawfordScott@gmail.com>
2018-01-02 13:42:55 -0500

    Adds session reducers;

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,7 +1,7 @@
 import * as APIUtil from '.././util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECIEVE_CURRENT_USER';
-export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 const receiveCurrentUser = (currentUser) => {
   return {
@@ -12,7 +12,7 @@ const receiveCurrentUser = (currentUser) => {
 
 const receiveErrors = (errors) => {
   return {
-    type: RECEIVE_ERRORS,
+    type: RECEIVE_SESSION_ERRORS,
     errors
   };
 };
