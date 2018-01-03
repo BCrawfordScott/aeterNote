71a0e975d7e2c5327368eb0692db30c7be646a2f
BCrawfordScott <BrianCrawfordScott@gmail.com>
2018-01-03 14:09:17 -0500

    Finish homepage and session form styling

---
+++ frontend/components/navbars/footer_nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FooterNav = () => {
+  return(
+    <div className='foot-nav'>
+      <div className='foot-nav-elements'>
+
+      </div>
+    </div>
+  );
+};
+
+export default FooterNav;
--- frontend/components/navbars/top_nav.jsx
+++ frontend/components/navbars/top_nav.jsx
@@ -4,11 +4,13 @@ import SessionLinks from '../session/session_links';
 const TopNav = () => {
   return(
     <div className='top-nav'>
-      <div className='logo'>
-        <img  src={window.staticImages.logoMain} />
-        <h3 >ÆTERNOTE</h3>
+      <div className='nav-elements'>
+        <div className='logo'>
+          <img  src={window.staticImages.logoMain} />
+          <h3 >ÆTERNOTE</h3>
+        </div>
+        <SessionLinks/>
       </div>
-      <SessionLinks/>
     </div>
   );
 };
