8f799bab3443aac524f10631864509562c8f3207
BCrawfordScott <BrianCrawfordScott@gmail.com>
2018-01-11 13:46:03 -0500

    Finishes note error styling

--- frontend/reducers/notes_error_reducer.jsx
+++ frontend/reducers/notes_error_reducer.jsx
@@ -1,6 +1,8 @@
 import {
   RECEIVE_NOTE_ERRORS,
-  REMOVE_NOTE_ERRORS
+  REMOVE_NOTE_ERRORS,
+  RECEIVE_NOTE,
+  RECEIVE_NOTES
 } from '../actions/note_actions';
 import { LOGOUT } from '../actions/session_actions';
 
@@ -12,6 +14,8 @@ const notesErrorReducer = (state = [], action) => {
     case(RECEIVE_NOTE_ERRORS):
       return action.errors;
     case(REMOVE_NOTE_ERRORS):
+    case(RECEIVE_NOTE):
+    case(RECEIVE_NOTES):
     case(LOGOUT):
       return [];
     default:
