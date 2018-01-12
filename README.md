333a61db1fac0b7ef5846ed8b9bf93a4928c1d80
BCrawfordScott <BrianCrawfordScott@gmail.com>
2018-01-12 10:27:04 -0500

    Adds readme images

--- README.md
+++ README.md
@@ -1,24 +1,50 @@
-# README
+# æterNote
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Welcome to [æterNote](https://aeternote.herokuapp.com/), a single-page clone of [Evernote](https://evernote.com/).  æterNote is a web application for creating and organizing rich text formatted notes quickly and easily online.
 
-Things you may want to cover:
+### Technologies
+æterNote uses React, Redux, and ES6 to dynamically generate the frontend, and Ruby on Rails for data management on the backend. No jQuery was used except to generate ajax requests to the Rails backend.
 
-* Ruby version
+## Features
 
-* System dependencies
+### User Authentication
+Users can securely sign up, log in, and log out simply by setting an email address and password for their account.
 
-* Configuration
+*sessionimage*
 
-* Database creation
+### Structure
+æterNote's main components are as follows:
 
-* Database initialization
+1. Sidenav
+  - Main navigation for the application
+2. Note Show
+  - Serves as both the creator and editor of a users notes
+  - Implemented through [React Quill](https://github.com/zenoamaro/react-quill)
+3. Notebooks
+  - Lists all notebooks
+  - Shows associated notes
+4. Tags
+  - Lists all tags
+  - Shows associated notes
 
-* How to run the test suite
+### Rich Text Editing
+æterNote implements React-Quill, a react component version of the popular Quill.js
 
-* Services (job queues, cache servers, search engines, etc.)
+*texteditorimage*
 
-* Deployment instructions
+The editor serves as both a create and/or an update form for notes.  It gives the user the ability to assign and re-assign the current note to notebooks, and assign tags to the current note.
 
-* ...
+Notebooks are listed via dropdown at the top of the editor. Users can also create a new notebook through the dropdown.
+
+Tags are listed as clickable buttons in a list at the top of the editor.
+
+### Easy UI
+React and redux architecture provide a simple and intuitive interface.  The UI id present and recognizable throughout the entire application and gives users the ability to navigate to any organizational component from any other organizational component.
+
+*uiimgages*
+
+#### Future Features
+
+1. Auto-save
+2. Image uploading
+3. Google maps integration
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,14 +14,36 @@ User.create!([
   {email: 'pt@barnum.com', password: 'circus'}
 ])
 
-Notebook.create!([
-  {title: "History", user_id: User.last.id},
-  {title: "Animals", user_id: User.last.id},
-  {title: "Acts", user_id: User.last.id},
-  {title: "Competition", user_id: User.last.id}
-])
 
-Note.create!([
-  {title: "Summary", content: "Phineas Taylor Barnum (July 5, 1810 – April 7, 1891) was an American showman, politician, and businessman remembered for promoting celebrated hoaxes and for founding the Barnum & Bailey Circus (1871-2017)", user_id: User.last.id, notebook_id: User.last.notebooks.first.id },
-  {title: "Jenny Lind", content: "Says there was never an affair...yeah right.", user_id: User.last.id, notebook_id: User.last.notebooks.first.id }
-])
+history = Notebook.create!({title: "History", user_id: User.last.id})
+animals = Notebook.create!({title: "Animals", user_id: User.last.id})
+acts = Notebook.create!({title: "Acts", user_id: User.last.id})
+competition = Notebook.create!({title: "Competition", user_id: User.last.id})
+presentation = Notebook.create!({title: "Presentation", user_id: User.last.id})
+
+
+aeternote = Note.create({title: 'æterNote', content: '', user_id: User.last.id, notebook_id: presentation.id})
+summary = Note.create!({title: "Summary", content: "", user_id: User.last.id, notebook_id: history.id })
+jenny_lind = Note.create!({title: "Jenny Lind", content: "", user_id: User.last.id, notebook_id: acts.id })
+elephants = Note.create!({title: 'Elephants', content: '', user_id: User.last.id, notebook_id: animals.id})
+monkeys = Note.create!({title: 'Monkeys', content: '', user_id: User.last.id, notebook_id: animals.id})
+lions = Note.create!({title: 'Lions', content: '', user_id: User.last.id, notebook_id: animals.id})
+high_wire = Note.create!({title: 'High Wire', content: '', user_id: User.last.id, notebook_id: acts.id})
+globe_of_death = Note.create!({title: 'Globe of Death', content: '', user_id: User.last.id, notebook_id: acts.id})
+greatest_show_on_earth = Note.create!({title: "Barnum and Bailey's Greatest Show on Earth", content: '', user_id: User.last.id, notebook_id: history.id})
+ringling_bros = Note.create!({title: "Ringling_Brothers", content: '', user_id: User.last.id, notebook_id: competition.id})
+
+fun = Tag.create!({label: 'Fun', user_id: User.last.id})
+boring = Tag.create!({label: 'Boring', user_id: User.last.id})
+exciting = Tag.create!({label: 'Exciting', user_id: User.last.id})
+work = Tag.create!({label: 'Work', user_id: User.last.id})
+
+Tagging.create!({note_id: aeternote.id, tag_id: work.id})
+Tagging.create!({note_id: summary.id, tag_id: boring.id})
+Tagging.create!({note_id: elephants.id, tag_id: exciting.id})
+Tagging.create!({note_id: monkeys.id, tag_id: exciting.id})
+Tagging.create!({note_id: highwire.id, tag_id: exciting.id})
+Tagging.create!({note_id: globe_of_death.id, tag_id: exciting.id})
+Tagging.create!({note_id: jenny_lind.id, tag_id: boring.id})
+Tagging.create!({note_id: aeternote.id, tag_id: fun.id})
+Tagging.create!({note_id: ringling_bros.id, tag_id: work.id})
