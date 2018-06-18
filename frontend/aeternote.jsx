f70da991de46e3ae7618545de64cd6d145c31a16
BCrawfordScott <BrianCrawfordScott@gmail.com>
2018-06-18 19:52:33 -0400

    remove testers and logger

--- frontend/aeternote.jsx
+++ frontend/aeternote.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store.js';
 import Root from './components/root';
 
-//TESTING//
-
-//TESTING//
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -24,9 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore();
   }
   const root = document.getElementById('root');
-
-  window.getState = store.getState; //TESTING//
-  window.dispatch = store.dispatch; //TESTING//
 
   ReactDOM.render(<Root store={ store } />, root);
 });
